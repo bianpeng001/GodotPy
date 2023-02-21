7cbbb5295cf8c1bbd6acdac1001f797c258ec27e
bianpeng <bianpeng001@163.com>
2023-02-21 09:56:42 +0800

    add publish

--- Godot/build_windows.bat
+++ Godot/build_windows.bat
@@ -1,7 +1,9 @@
 @rem scons -j6 platform=windows vsproj=yes bits=64 target=editor
 
 goto :gd
-goto :mono
+@rem goto :mono
+@rem goto :publish
+goto :end
 
 :mono
 scons p=windows bits=64 target=editor module_mono_enabled=yes
@@ -17,6 +19,15 @@ scons p=windows vsproj=yes bits=64 -j6 target=editor dev_build=true
 @rem scons p=windows tools=no bits=64 -j6 target=template_release
 goto :end
 
+
+:publish
+set WORK_DIR=d:\OpenSource\GodotPy
+scons p=windows tools=no bits=64 -j6 target=template_release
+bin\godot.windows.editor.dev.x86_64.console.exe --path %WORK_DIR%\Demo -w --export-pack "Windows Desktop" %WORK_DIR%\Build\Demo.pck
+copy /Y bin\python3.dll %WORK_DIR%\Build\python3.dll
+copy /Y bin\godot.windows.template_release.x86_64.exe %WORK_DIR%\Build\Demo.exe
+
+goto :end
 
 :end
 
