ac12bb78ee259de604004efd8e0ad313f9f2c468
bianpeng <bianpeng001@163.com>
2023-03-04 20:20:27 +0800

    xx

--- Demo/game/ui/build_panel_controller.py
+++ Demo/game/ui/build_panel_controller.py
@@ -19,11 +19,14 @@ class BuildPanelController(UIController, PopupTrait):
 
         self.ui_obj.find_node('HBoxContainer/Left/BtnClose').connect(PRESSED, self.on_close_click)
         self.ui_obj.find_node('HBoxContainer/BtnFarm').connect(PRESSED, self.on_build_farm_click)
+        self.ui_obj.find_node('HBoxContainer/BtnWorkHouse').connect(PRESSED, self.on_build_work_house_click)
 
         game_mgr.event_mgr.add(SCENE_GROUND_CLICK, self.on_scene_ground_click)
 
         farm_obj = FNode3D.instantiate('res://models/Farm01.tscn')
+        work_house_obj = FNode3D.instantiate('res://models/WorkHouse01.tscn')
         self.item_list.append(farm_obj)
+        self.item_list.append(work_house_obj)
 
         for item in self.item_list:
             item.set_visible(False)
@@ -31,6 +34,12 @@ class BuildPanelController(UIController, PopupTrait):
     # 是否正在建设激活状态
     def is_building(self):
         return self.is_visible and self.active_obj
+
+    def set_active_obj(self, obj):
+        if self.active_obj:
+            self.active_obj.set_visible(False)
+        self.active_obj = obj
+        self.active_obj.set_visible(True)
     
     def on_scene_ground_click(self):
         if self.is_building():
@@ -43,8 +52,10 @@ class BuildPanelController(UIController, PopupTrait):
         self.active_obj = None
 
     def on_build_farm_click(self):
-        self.active_obj = self.item_list[0]
-        self.active_obj.set_visible(True)
+        self.set_active_obj(self.item_list[0])
+        
+    def on_build_work_house_click(self):
+        self.set_active_obj(self.item_list[1])
 
     def update(self):
         if self.is_visible and self.active_obj:
