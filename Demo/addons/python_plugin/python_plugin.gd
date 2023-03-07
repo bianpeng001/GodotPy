aabe1228af7f5cc96debf9182923a5812dd4cf43
bianpeng <bianpeng001@163.com>
2023-03-07 14:33:17 +0800

    add editor plugin

--- Demo/addons/python_plugin/python_plugin.gd
+++ Demo/addons/python_plugin/python_plugin.gd
@@ -1,12 +1,14 @@
 @tool
 extends EditorPlugin
 
-
 func _enter_tree():
-	print('hello python plugin')
-	pass
+	print('init python plugin')
+	add_tool_menu_item("Exec Python", self.on_exec_python)
 
+func on_exec_python():
+	print('exec python')
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
+	remove_tool_menu_item("Exec Python")
 	pass
