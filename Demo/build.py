d80e5c3839b926897b729cec9d14beeffa0813a0
bianpeng <bianpeng001@163.com>
2023-03-10 10:23:11 +0800

    build script ok

--- Demo/build.py
+++ Demo/build.py
@@ -1,7 +1,56 @@
 #
-# 2023年2月21日 bianpeng
+# 2023年3月10日 bianpeng
 #
+# build script
 
-print('build')
+import os
+import os.path
+import shutil
+import glob
 
+GODOT_DIR = 'D:\\OpenSource\\godot'
+if not os.path.exists(GODOT_DIR):
+    GODOT_DIR = ''
+
+WORK_DIR = 'D:\\OpenSource\\GodotPy'
+DEMO_DIR = os.path.join(WORK_DIR, 'Demo')
+BUILD_DIR = os.path.join(WORK_DIR, 'Build')
+
+EDITOR = os.path.join(GODOT_DIR, 'bin', 'godot.windows.editor.x86_64.exe')
+PLAYER = os.path.join(GODOT_DIR, 'bin', 'godot.windows.template_release.x86_64.exe')
+
+RES_HACKER = f'd:\Tools\ResHacker\ResourceHacker.exe'
+SCONS_EXE = 'scons.exe'
+
+def run(cmd):
+    print(cmd)
+    os.system(cmd)
+
+def main():
+    os.chdir(GODOT_DIR)
+
+    run(f'{SCONS_EXE} p=windows vsproj=no bits=64 -j6 target=editor dev_build=false')
+    run(f'{EDITOR} -w --path "{DEMO_DIR}" --export-pack "Windows Desktop" {BUILD_DIR}\\Demo.pck')
+    run(f'{SCONS_EXE} p=windows tools=no bits=64 -j6 target=template_release')
+
+    file_list = (
+        'python.exe',
+        'python3.dll',
+        'sqlite3.dll',
+        '_sqlite3.pyd',
+    )
+    for item in file_list:
+        shutil.copy(os.path.join(GODOT_DIR, 'bin', item), os.path.join(BUILD_DIR, item))
+
+    shutil.copy(EDITOR, os.path.join(BUILD_DIR, 'GodotEditor.exe'))
+    shutil.copy(PLAYER, os.path.join(BUILD_DIR, 'Demo.exe'))
+    
+    run(f'{RES_HACKER} -script {WORK_DIR}\\Godot\\replace_icon.txt')
+
+    # remove pyc
+    for f in glob.iglob(f'{DEMO_DIR}\\game\\**\\*.pyc'):
+        os.remove(f)
+
+if __name__ == '__main__':
+    main()
 
--- Demo/game/boot.py
+++ Demo/game/boot.py
@@ -1,23 +1,13 @@
 #
 # 2023年2月1日 bianpeng
 #
-from game.core import print_line, log_util
+from game.core import print_line
 import sys
 
 # saved_stderr = sys.stderr
 # saved_stdout = sys.stdout
 
 # init stdout, stderr
-class PrintLine0:
-    def write(self, s):
-        if s and s != '\n' and s != ' ' and s != '^':
-            print_line(s)
-            #print_line(repr(s))
-            pass
-
-    def flush(self):
-        pass
-
 class PrintLine:
     def __init__(self):
         self.sb = []
@@ -32,9 +22,10 @@ class PrintLine:
     
     def flush(self):
         pass
-        
-sys.stderr = PrintLine()
-sys.stdout = PrintLine()
 
-log_util.debug('boot ok')
+if __name__ != '__main__':
+    sys.stderr = PrintLine()
+    sys.stdout = PrintLine()
+
+print('boot ok')
 
