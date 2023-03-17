e6ce8f1d330e9b1a310a4770b869dd04469828ff
bianpeng <bianpeng001@163.com>
2023-03-17 13:48:13 +0800

    build.py ok

--- Demo/build.py
+++ Demo/build.py
@@ -7,14 +7,17 @@ import os
 import os.path
 import shutil
 import glob
+import zipfile
 
 GODOT_DIR = 'D:\\OpenSource\\godot'
 PROJECT_DIR = 'D:\\OpenSource\\GodotPy'
+PYTHON_DIR = 'D:\\OpenSource\\cpython'
 THREADS = 6
 
 if not os.path.exists(GODOT_DIR):
     GODOT_DIR = f'H:\\godot'
     PROJECT_DIR = f'H:\\GodotPy'
+    PYTHON_DIR = 'H:\\cpython'
     THREADS = 4
 
 GODOT_BIN_DIR = os.path.join(GODOT_DIR, 'bin')
@@ -56,9 +59,12 @@ def build_publish():
     # replace app icon
     run(f'{RES_HACKER} -script {PROJECT_DIR}\\Godot\\replace_icon.txt')
 
+    make_python_zip()
+
     # remove pyc
     for f in glob.iglob(f'{DEMO_DIR}\\game\\**\\*.pyc'):
         os.remove(f)
+    archive_demo()
 
 def build_editor_debug():
     run(f'{SCONS_EXE} p=windows vsproj=yes tools=yes bits=64 -j{THREADS} target=editor dev_build=true')
@@ -72,6 +78,31 @@ def run_editor_release():
 def play_editor_debug():
     run(f'{EDITOR_DEBUG} -w --path {DEMO_DIR}')
 
+def zipdir(dir_path, f):
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if not file.endswith('.pyc'):
+                file_path = os.path.join(root, file)
+                f.write(file_path, os.path.relpath(file_path, dir_path))
+
+def make_python_zip():
+    path = f'{BUILD_DIR}\\python312.zip'
+    if os.path.exists(path):
+        os.remove(path)
+
+    with zipfile.ZipFile(path, 'w', zipfile.ZIP_DEFLATED) as f:
+        zipdir(f'{PYTHON_DIR}\\Lib', f)
+
+def archive_demo():
+    path = f'{PROJECT_DIR}\\Demo.zip'
+    if os.path.exists(path):
+        os.remove(path)
+
+    with zipfile.ZipFile(path, 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as f:
+        zipdir(BUILD_DIR, f)
+
+    os.rename(path, f'{BUILD_DIR}\\Demo.zip')
+
 if __name__ == '__main__':
     import sys
 
@@ -81,6 +112,8 @@ if __name__ == '__main__':
         'editor_release' : build_editor_release,
         'run_editor_release' : run_editor_release,
         'play_editor_debug' : play_editor_debug,
+        'python_zip' : make_python_zip,
+        'archive_demo' : archive_demo,
     }
     fun = None
     if len(sys.argv) >= 2:
