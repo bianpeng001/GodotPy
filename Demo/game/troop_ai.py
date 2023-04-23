2bf7270706083701eee1e61526794f5a5b14bac7
bianpeng <bianpeng001@163.com>
2023-04-23 15:37:19 +0800

    see 9 tile units

--- Demo/game/troop_ai.py
+++ Demo/game/troop_ai.py
@@ -97,9 +97,9 @@ class StepMoveReq(MoveComponent):
         if d.sqr_magnitude() > 0.00005:
             new_pos = cur_pos + d
             
-            if self.block_count == 0:
-                controller.look_at(new_pos.x,new_pos.y,new_pos.z)
-            #controller.look_at(new_pos.x,new_pos.y,new_pos.z)
+            # if self.block_count == 0:
+            #     controller.look_at(new_pos.x,new_pos.y,new_pos.z)
+            controller.look_at(new_pos.x,new_pos.y,new_pos.z)
             troop.set_position(new_pos.x,new_pos.y,new_pos.z)
             
             if self.block_count > 0:
