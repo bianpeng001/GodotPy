7173d28be2d55fc4aa15398c54111b9292fdbdc1
bianpeng <bianpeng001@163.com>
2023-05-06 10:55:38 +0800

    xx

--- Demo/game/base_type.py
+++ Demo/game/base_type.py
@@ -269,7 +269,7 @@ class UIController:
 # 本方法在UI可见时才生效, 用于事件响应
 #
 def WhenVisible(fun):
-    def _fun(self, *args,**kwargs):
+    def _fun(self, *args, **kwargs):
         if self.is_show():
             return fun(self, *args, **kwargs)
     
