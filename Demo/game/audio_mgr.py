afb81f113f30ca763ec25ad9bf5802f83b741874
bianpeng <bianpeng001@163.com>
2023-08-25 16:14:41 +0800

    two audio player

--- Demo/game/audio_mgr.py
+++ Demo/game/audio_mgr.py
@@ -6,30 +6,41 @@
 from game.core import *
 from game.game_mgr import *
 
+class AudioItem:
+    def __init__(self, path, volume_db):
+        self.path = path
+        self.volume_db = volume_db
+        self.res_capsule = None
+
 #
 # 声音播放
 #
 class AudioMgr:
     def __init__(self):
+        # 短音效
         self.player = None
+        # 背景音乐, 需要一直播放
+        self.bgm_player = None
+        self.music_list = []
 
     def init(self):
-        self.player = game_mgr.scene_root_obj.find_node('AudioStreamPlayer')
+        self.player = game_mgr.scene_root_obj.find_node('AudioMgr/AudioStreamPlayer')
+        self.bgm_player = game_mgr.scene_root_obj.find_node('AudioMgr/AudioStreamPlayer2')
         log_debug(self.player)
 
-        self.game_start_sound = ResCapsule.load_resource('res://sound/game-complete.wav')
-        self.ground_click_sound = ResCapsule.load_resource('res://sound/ground-click.wav')
+        game_start_sound = ResCapsule.load_resource('res://sound/game-complete.wav')
+        ground_click_sound = ResCapsule.load_resource('res://sound/ground-click.wav')
+
+        self.music_list = [
+            game_start_sound,
+            ground_click_sound,
+        ]
 
     def play_sound(self, id):
-        match id:
-            case 0:
-                self.player.set_stream(self.game_start_sound.res)
-            case 1:
-                self.player.set_stream(self.ground_click_sound.res)
+        self.player.set_stream(self.music_list[id].res)
         self.player.play(0)
         self.player.set_volume(-20)
 
     def cleanup(self):
-        self.game_start_sound = None
-        self.ground_click_sound = None
+        self.music_list = None
 
