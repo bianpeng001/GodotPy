328ae97cfc54609244fceb056f0f79cde8b55e70
bianpeng <bianpeng001@163.com>
2023-08-26 23:54:00 +0800

    c++ binding

--- Godot/modules/GodotPy/src/Binding.cpp
+++ Godot/modules/GodotPy/src/Binding.cpp
@@ -11,6 +11,28 @@ template<> const char *TypeLabel<char*>::Label = "s";
 template<> const char *TypeLabel<int>::Label = "i";
 template<> const char *TypeLabel<PyObject*>::Label = "o";
 
+//
+// parse args tuple
+//
+template<typename T>
+void GetTupleItem(PyObject* arg, T* Result) { }
+template<> void GetTupleItem<int>(PyObject *arg, int *Result) {
+    long ivalue = PyLong_AsLong(arg);
+
+    *Result = (int)ivalue;
+}
+typedef const char* cchar_ptr;
+template<> void GetTupleItem<cchar_ptr>(PyObject *arg, cchar_ptr *Result) {
+    const char *sarg;
+    Py_ssize_t len;
+
+    sarg = PyUnicode_AsUTF8AndSize(arg, &len);
+    *Result = sarg;
+}
+template<> void GetTupleItem<PyObject*>(PyObject *arg, PyObject **Result) {
+    *Result = arg;
+}
+
 template<typename T>
 struct Arg {
     typedef T Type;
