04a1c7c2f8a9ac012de40ce5b405ffc4c3632a52
bianpeng <bianpeng001@163.com>
2024-05-16 14:35:36 +0800

    add executable path to sys.path

--- Godot/modules/GodotPy/src/GodotPy.cpp
+++ Godot/modules/GodotPy/src/GodotPy.cpp
@@ -3366,12 +3366,12 @@ static int InitPython() {
 	if (project_path.ends_with("/")) {
 		project_path = project_path.substr(0, project_path.length() - 1);
 	}
-	project_path = project_path;
 
 	String bin_dir = OS::get_singleton()->get_executable_path().get_base_dir();
 	String Lib = bin_dir + "/Lib";
 	String DLLs = bin_dir + "/DLLs";
 	String LibZip = bin_dir + "/python313.zip";
+	print_line(bin_dir);
 	print_line(Lib);
 	print_line(DLLs);
 	print_line(project_path);
@@ -3384,16 +3384,19 @@ static int InitPython() {
 	memset(Buff.data(), 0, sizeof(CHAR)*Buff.size());
 	*/
 
+	auto BinPath = bin_dir.to_wchar_buffer();
 	auto LibPath = Lib.to_wchar_buffer();
 	//auto DLLsPath = DLLs.to_wchar_buffer();
 	//auto ProjectPath = project_path.to_wchar_buffer();
 	auto LibZipPath = LibZip.to_wchar_buffer();
 
+	const wchar_t *PtrBinPath = (const wchar_t *)BinPath.ptr();
 	const wchar_t *PtrLibPath = (const wchar_t *)LibPath.ptr();
 	//const wchar_t *PtrDLLsPath = (const wchar_t *)DLLsPath.ptr();
 	//const wchar_t *PtrProjectPath = (const wchar_t *)ProjectPath.ptr();
 	const wchar_t *PtrLibZipPath = (const wchar_t *)LibZipPath.ptr();
 
+	PyWideStringList_Append(&PyConfig.module_search_paths, PtrBinPath);
 	PyWideStringList_Append(&PyConfig.module_search_paths, PtrLibPath);
 	//PyWideStringList_Append(&PyConfig.module_search_paths, PtrDLLsPath);
 	//PyWideStringList_Append(&PyConfig.module_search_paths, PtrProjectPath);
