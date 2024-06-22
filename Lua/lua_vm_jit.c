d1e6906742b85c02c32ba23068738fe7d45e997d
bianpeng <bianpeng001@163.com>
2024-06-22 15:25:07 +0800

    jit

--- Lua/lua_vm_jit.c
+++ Lua/lua_vm_jit.c
@@ -1,7 +1,56 @@
 /*
 ** bianpeng 2024-6-21
-** big day!!
+** begin to implement a simple jit, use c cross platform in c language, for a test
+** without any asm jit framework
 */
 
+#include "lua.h"
+
+typedef struct _TInstruction
+{
+    int FuncID;
+} TInstruction;
+
+typedef struct _TInstructionA
+{
+    struct _TInstruction Inst;
+    int a;
+} TInstructionA;
+
+typedef void (*TInstructFuntion)(TInstruction* pInstruct);
+
+
+static void Add(TInstructionA* pInstruct)
+{
+}
+
+
+static TInstructFuntion InstructionFuncTable[] = 
+{
+    (TInstructFuntion)&Add,
+    (TInstructFuntion)&Add,
+    (TInstructFuntion)&Add,
+    (TInstructFuntion)&Add,
+    (TInstructFuntion)&Add,
+    NULL,
+};
+
+static inline void ExecuteOne(TInstruction* pInstruct)
+{
+    TInstructFuntion Fun = InstructionFuncTable[pInstruct->FuncID];
+    if (Fun)
+    {
+        Fun(pInstruct);
+    }
+
+}
+
+void Execute(TInstruction *pInstruct, int start, int stop)
+{
+    for(int i = start; i < stop; ++i)
+    {
+        ExecuteOne(pInstruct + i);
+    }
+}
 
 
