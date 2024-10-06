9375ae6d25d4abb9671af15a529537b958f8eccc
bianpeng <bianpeng001@163.com>
2024-10-06 19:00:45 +0800

    add start screen

--- Demo/start.gd
+++ Demo/start.gd
@@ -1,7 +1,19 @@
 extends Node
 
+var timer = Timer.new()
+
 #
 # 先直接场景
 #
 func _ready():
-	get_tree().change_scene_to_file('res://campfire_scene.tscn')
+	timer.connect("timeout", self.enter_game)
+	timer.wait_time = 1
+	timer.one_shot = true
+	self.add_child(timer)
+	timer.start()
+	
+	
+func enter_game():
+	get_tree().change_scene_to_file.bind('res://campfire_scene.tscn').call_deferred()
+	
+	
